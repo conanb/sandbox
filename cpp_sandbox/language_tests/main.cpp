0cf1f7e3f873f57b793758668a92d3b5c6c484ac
Conan Bourke <conan.bourke@unsw.edu.au>
2022-05-06 16:50:52 +1000

    just testing vs cmake support and some basic c++ auto use cases

---
+++ cpp_sandbox/language_tests/main.cpp
@@ -0,0 +1,77 @@
+// this is literally just a test of "modern" c++
+// i've used/taught a lot of this in the past, but it's always
+// good to rehash knowledge
+#include <iostream>
+#include <vector>
+#include <string>
+
+// needed for the string litteral s, i.e. auto str = "Hello"s;
+using namespace std::string_literals;
+
+template <typename T>
+auto add(T a, T b) {
+	return a + b;
+}
+
+struct {
+	template <typename T, typename U>
+	auto operator () (T const a, U const b) const { return a + b; }
+} L;
+
+template <typename T, int COUNT>
+struct vec
+{
+	T v[COUNT];
+};
+
+int main(int argc, char* argv[]) {
+
+	auto i = 42;
+	auto f = 42.f;
+	auto d = 42.0;
+	auto s = "Blep";
+	auto a = { 1, 2, 3, 4 };
+
+	auto l = []() { std::cout << "Blag" << std::endl; };
+	l();
+
+	auto ii = add(5, 9);
+	auto dd = add(5.0, 9.0);
+
+	auto add = [](auto const a, auto const b) {return a + b; };
+
+	auto ii2 = add(5, 9);
+	auto id = add(8, 9.0);
+
+	// use auto because .size() is a size_t not int etc
+	// sure, could use unsigned int, but why bother?
+	auto v = std::vector<int>{ 1, 2, 3, 4 };
+	auto sz = v.size();
+
+	// note: auto doesn't work for non-moveable types
+	//auto at = std::atomic<int>(42);
+
+	// not possible with multi-word types
+	//auto ll = long long(42);
+	using llong = long long;
+	auto ll = llong(42); // all good
+	auto ll2 = 42LL; // also ok because of compiler specifying LL type
+
+	auto la = L("forty six"s, " and two"s);
+
+	using uint = unsigned int;
+	using byte = unsigned char;
+	using array_10i = int[10];
+
+	array_10i arr10 = {};
+	std::cout << arr10[0] << ' ' << arr10[10] << std::endl;
+	auto arr = new array_10i;
+
+	// type aliases
+	using vec2f = vec<float, 2>();
+	using vec3f = vec<float, 3>();
+	using vec4f = vec<float, 4>();
+	using vec4d = vec<double, 4>();
+
+	return 0;
+}
