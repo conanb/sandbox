4b5e7f0b79c7dbbf9e1f17ce4a5747a7744ee3fb
Conan Bourke <conan.bourke@unsw.edu.au>
2022-05-10 16:43:28 +1000

    c++11-17 tweaks

--- cpp_sandbox/language_tests/main.cpp
+++ cpp_sandbox/language_tests/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstddef>
+#include <map>
 
 // needed for the string litteral s, i.e. auto str = "Hello"s;
 using namespace std::string_literals;
@@ -26,6 +28,37 @@ struct vec
 
 void foo(int a, int b, int c) {}
 void foo(std::initializer_list<int> l) {}
+
+// a reminder on data padding
+struct padded_bad {
+	char c;
+	// extra 3 bytes padding here!
+	float f;
+	short s;
+	// extra 2 bytes padding here!
+	int i;
+};
+struct padded_better {
+	float f;
+	int i;
+	short s;
+	char c;
+	// extra 1 byte padding here
+};
+
+// force padding
+struct alignas(4) padded_char {
+	char c;
+	// extra 3 bytes padding
+	char c2;
+	// extra 3 bytes padding
+};
+
+// final 
+class Brand {};
+class CarBrand : Brand {};
+class Subaru final : CarBrand {};
+// class FunkySubaru : Subaru {};
 
 int main(int argc, char* argv[]) {
 
@@ -69,7 +102,7 @@ int main(int argc, char* argv[]) {
 	array_10i arr10 = {};
 	// VS gives me a warning that arr10[10] is reading from an invalid source, 
 	// so that's awesome!
-	std::cout << arr10[0] << ' ' << arr10[10] << std::endl;
+	std::cout << arr10[0] << ' ' << arr10[9] << std::endl;
 	auto arr = new array_10i;
 
 	// type aliases
@@ -80,6 +113,56 @@ int main(int argc, char* argv[]) {
 
 	// initialiser list is a little different to what I remember
 	foo({ 1, 2, 3 });
+
+	// vec with size 5
+	std::vector<int> v1(5);
+	// vec with a single int added with value 5
+	std::vector<int> v2{ 5 };
+
+	auto j = { 5 }; // std::initializer_list copy, so auto becomes std::initializer_list<int>
+	auto k{ 5 }; // deduction, not std::initializer_list in c++17+
+
+	auto lis = { 5, 6 }; // std::initializer_list copy
+	// auto m{ 5, 6 }; // not posible since it isn't using a std::initializer_list
+
+	// for some reason this is missing
+	std::byte x = (std::byte)10;
+
+	 // good ol scoped enums for clearer code
+	enum class Result { Success, Failure, Ongoing };
+	Result result = Result::Ongoing;
+	// or using...
+	using Result::Ongoing;
+	Result result2 = Ongoing;
+
+	// this is great!
+	switch (result)
+	{
+		// with using you don't need to declare enum scope constantly
+		using enum Result;
+		case Success:
+		case Failure:
+		case Ongoing:
+		default:
+			break;
+	}
+
+	// fun way to expose enums within class
+	class Behaviour {
+	public:
+		enum class Result { Success, Failure, Ongoing };
+		using enum Result;
+	};
+	auto g_thing = Behaviour::Failure;
+
+	std::map<std::string, int> ma{
+		{"Conan", 42},
+		{"Cheaky", 69}
+	};
+	// ok I'm liking this...
+	for (auto&& [key, value] : ma) {
+		std::cout << key << ' ' << value << std::endl;
+	}
 
 	return 0;
 }
